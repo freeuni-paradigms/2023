4f7d9c307292d6aa7daa22ba4733fe7c2c576e66
tamtatop <ttopu18@freeuni.edu.ge>
2023-10-06 15:12:05 +0400

    refactor: rm fn content

--- Seminars/Tamta/06/Strings/strings_template.c
+++ Seminars/Tamta/06/Strings/strings_template.c
@@ -3,12 +3,7 @@
 
 int strcmp(const char *s1, const char *s2)
 {
-    while(*s1 != 0 && *s2 != 0 && *s1 == *s2) {
-        s1++;
-        s2++;
-    }
-    return *(unsigned char*)s1 - *(unsigned char*)s2;
-    // return *s1 - *s2;
+   
 }
 
 char *strcpy(char *dest, const char *src)
