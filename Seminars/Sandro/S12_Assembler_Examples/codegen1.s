a1871c0945f0e0949b3bc2decd3bbca8f01bf176
Aleksandre Khokhiashvili <alkhok18@freeuni.edu.ge>
2023-11-03 11:42:34 +0400

    asm

--- Seminars/Sandro/S12_Assembler_Examples/codegen1.s
+++ Seminars/Sandro/S12_Assembler_Examples/codegen1.s
@@ -3,22 +3,24 @@ main:
 # grow stack
 addi sp, sp, -32
 
-# i = 6
-li x10, 6
-sw x10, 0(sp)
-# i ++
-lw x10, 0(sp) # x10 = *(int*)(sp+0)
-addi x10, x10, 1 # x10 = x10 + 1
+# i = 6 i is stored at addr sp
+
+li x10, 6 # x10 = 6
 sw x10, 0(sp) # *(int*)(sp+0) = x10
 
-# x11 = i
+# i++
+lw x10, 0(sp) 
+# x10=x10+1
+addi x10, x10, 1 # x10 = x10 + 1
+sw x10, 0(sp)
+
 lw x11, 0(sp)
 
 # shrink stack
 addi sp, sp, 32
 
 # print(x11) print=1
-li x10, 1
+li x10, 1 # x10 = 1-> print
 ecall
 
 # exit() exit=10
